6d81575f031866551e14a7adea376945e5cb3704
ななみん <119144138+nanamichan0405@users.noreply.github.com>
2023-01-09 20:08:08 +0900

    条件分岐編
    JS基礎11課題

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ document.getElementById("lists")
 pullDownButton.addEventListener('click',function(){
 pullDownButton.setAttribute("style","background-color:green;")
 })
+
+//js11↓
+const pullDownParents = document.getElementById("pull-down")
+pullDownButton.addEventListener('click', function(){
+    if(pullDownParents.getAttribute("style")=="display:block;"){
+pullDownParents.removeAttribute("style", "display:block;")
+console.log("非表示")
+    }else{
+    pullDownParents.setAttribute("style", "display:block;")
+    console.log("表示")
+}
+    })
